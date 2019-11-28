d7003c431a5fa6b3abfc41ebebe860a8904d381b
Christopher-B-D <57333631+Christopher-B-D@users.noreply.github.com>
2019-11-28 16:36:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.checkbox input[type=checkbox]').on('change', function() {
+    $(this).closest('.checkbox').toggleClass('checked', $(this).is(':checked'));
+});
+
+$('.switch input[type=checkbox]').on('change', function() {
+    $(this).closest('.switch').toggleClass('checked', $(this).is(':checked'));
+});
+
+$('.radio input[type=radio]').on('change', function() {
+    $(this).closest('.radio').toggleClass('checked', $(this).is(':checked'));
+});
